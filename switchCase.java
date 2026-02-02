d661a975d20030a3b1b01507aca4537d00bac263
himu0023 <himanshubisht3231@gmail.com>
2026-02-02 11:49:41 +0530

    switch case

--- switchCase.java
+++ switchCase.java
@@ -1,10 +1,26 @@
 public class switchCase {
     public static void main(String[] args) {
-        switch (args) {
-            case value:
-                
+        int movie = 4;
+        switch (movie) {
+            case 1:
+                System.out.println("Jack Reacher");
                 break;
-        
+
+            case 2:
+                System.out.println("Mission Impossible");
+                break;
+
+            case 3: 
+                System.out.println("Transformers");
+                break;
+
+            case 4:
+                System.out.println("Breaking Bad");
+                break;
+            
+            case 5: 
+                System.out.println("Game of Thrones");
+
             default:
                 break;
         }
