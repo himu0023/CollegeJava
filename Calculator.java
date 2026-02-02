85321a6ab07a166e318dc44f2f72b6cf253c82c4
himu0023 <himanshubisht3231@gmail.com>
2026-02-02 10:58:18 +0530

    simple arthmetic operations

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+    public static void main (String[] args){
+
+        int num1 = 20;
+        int num2 = 10;
+
+        int addition = num1 + num2; 
+        System.out.println("Addition: "+ addition);
+
+        int substraction = num1 - num2;
+        System.out.println("Substraction: " + substraction);
+
+        int multiplication = num1 * num2;
+        System.out.println(("Multiplication: " + multiplication));
+
+        int divide = num1/ num2;
+        System.out.println("Division: " + divide);
+    }
+}
