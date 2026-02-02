962192ae9646d08dee0dd03b4edb771917cef76a
himu0023 <himanshubisht3231@gmail.com>
2026-02-02 11:27:44 +0530

    conditionla statement

---
+++ Conditional.java
@@ -0,0 +1,15 @@
+public class Conditional {
+    public static void main (String[] args){
+        int marks = 89;
+
+        if (marks >= 90) {
+            System.out.println("Grade: A");
+        } else if (marks>= 80) {
+            System.out.println("Grade: B");
+        } else if (marks>= 70) {
+            System.out.println("Grade: C");
+        } else{
+            System.out.println("Fail");
+        }
+    }
+}
