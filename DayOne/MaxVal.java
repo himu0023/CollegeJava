a66466c7083b42358bd89ded3bb02e98b6ed2e71
himu0023 <himanshubisht3231@gmail.com>
2026-02-03 06:57:08 +0530

    maxval

---
+++ DayOne/MaxVal.java
@@ -0,0 +1,13 @@
+package DayOne;
+
+public class MaxVal {
+    public static void main(String[] args) {
+        int a = 89;
+        int b = 23;
+        if (a>b) {
+            System.out.println("a is greater.");
+        }else{
+            System.out.println("b is greater.");
+        }
+    }
+}
